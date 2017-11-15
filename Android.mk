cb44941bdd7bb8510485a1ee874242322a1e940d
Aurimas Liutikas <aurimas@google.com>
2017-11-14 18:28:31 -0800

    Migrate packages/apps/Car to aapt2
    Test: None
    Change-Id: I63a6e40dba61fdd3e5c6325fcb3e8c80c7db1be2

--- Android.mk
+++ Android.mk
@@ -20,8 +20,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
-    frameworks/support/car/res
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 LOCAL_PACKAGE_NAME := CarMediaApp
 
@@ -31,11 +30,11 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_PRIVILEGED_MODULE := true
 
-LOCAL_STATIC_JAVA_LIBRARIES += \
+LOCAL_USE_AAPT2 := true
+
+LOCAL_STATIC_ANDROID_LIBRARIES += \
     android-support-car \
     android-support-v4
-
-LOCAL_AAPT_FLAGS += --extra-packages android.support.car
 
 LOCAL_PROGUARD_ENABLED := disabled
 
