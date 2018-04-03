54a9df95716c26d7ae3a226ad506061fb9ed6e6c
Roberto Perez <robertoalexis@google.com>
2018-04-03 10:25:39 -0700

    Initial version of Content Forward Browsing in embedded.
    Bug: 73257264
    Test: To be implemented in b/77243935
    Change-Id: Ib7328b51f08fdb7788838432219df2b5f877dd3a

--- src/com/android/car/media/PlaybackFragment.java
+++ src/com/android/car/media/PlaybackFragment.java
@@ -1,12 +1,11 @@
 package com.android.car.media;
 
-import static java.security.AccessController.getContext;
-
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.PorterDuff;
+import android.media.browse.MediaBrowser;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -16,29 +15,29 @@ import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
-import com.android.car.apps.common.ImageUtils;
+import com.android.car.media.browse.BrowseAdapter;
+import com.android.car.media.browse.ContentForwardStrategy;
+import com.android.car.media.common.GridSpacingItemDecoration;
 import com.android.car.media.common.MediaItemMetadata;
+import com.android.car.media.common.MediaSource;
 import com.android.car.media.common.PlaybackControls;
 import com.android.car.media.common.PlaybackModel;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
-import androidx.car.widget.ListItem;
-import androidx.car.widget.ListItemAdapter;
-import androidx.car.widget.ListItemProvider;
 import androidx.car.widget.PagedListView;
-import androidx.car.widget.TextListItem;
 
 /**
  * A {@link Fragment} that implements both the playback and the content forward browsing experience.
  * It observes a {@link PlaybackModel} and updates its information depending on the currently
- * playing media source through the {@link MediaSession} API.
+ * playing media source through the {@link android.media.session.MediaSession} API.
  */
-public class PlaybackFragment extends Fragment {
+public class PlaybackFragment extends Fragment implements PlaybackModel.PlaybackObserver,
+        MediaSource.Observer, BrowseAdapter.Observer {
     private static final String TAG = "PlaybackFragment";
     private static final DateFormat TIME_FORMAT = new SimpleDateFormat("m:ss", Locale.US);
 
@@ -51,64 +50,18 @@ public class PlaybackFragment extends Fragment {
     private TextView mSubtitle;
     private SeekBar mSeekbar;
     private PagedListView mBrowseList;
-    private ListItemAdapter mMediaAdapter;
     private int mBackgroundRawImageSize;
     private float mBackgroundBlurRadius;
     private float mBackgroundBlurScale;
-
-    private PlaybackModel.PlaybackObserver mObserver = new PlaybackModel.PlaybackObserver() {
-        @Override
-        public void onPlaybackStateChanged() {
-            updateState();
-        }
-
-        @Override
-        public void onSourceChanged() {
-            updateState();
-            updateMetadata();
-            updateAccentColor();
-            updateBrowse();
-        }
-
-        @Override
-        public void onMetadataChanged() {
-            updateMetadata();
-        }
-    };
-
-    private ListItemProvider mMediaItemsProvider = new ListItemProvider() {
-        @Override
-        public ListItem get(int position) {
-            if (mModel == null || !mModel.hasQueue()) {
-                return null;
-            }
-            List<MediaItemMetadata> queue = mModel.getQueue();
-            if (position < 0 || position >= queue.size()) {
-                return null;
-            }
-            MediaItemMetadata item = queue.get(position);
-            TextListItem textListItem = new TextListItem(getContext());
-            textListItem.setTitle(item.getTitle().toString());
-            textListItem.setBody(item.getSubtitle().toString());
-            textListItem.setOnClickListener(v -> mModel.onSkipToQueueItem(item.getQueueId()));
-            return textListItem;
-        }
-
-        @Override
-        public int size() {
-            if (mModel == null || !mModel.hasQueue()) {
-                return 0;
-            }
-            return mModel.getQueue().size();
-        }
-    };
+    private MediaSource mMediaSource;
+    private BrowseAdapter mBrowseAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
             Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_playback, container, false);
         mModel = new PlaybackModel(getContext());
-        mModel.registerObserver(mObserver);
+        mModel.registerObserver(this);
         mAlbumBackground = view.findViewById(R.id.album_background);
         mPlaybackControls = view.findViewById(R.id.playback_controls);
         mPlaybackControls.setModel(mModel);
@@ -120,12 +73,14 @@ public class PlaybackFragment extends Fragment {
         mSeekbar = view.findViewById(R.id.seek_bar);
         mTime = view.findViewById(R.id.time);
         mBrowseList = view.findViewById(R.id.browse_list);
-        mMediaAdapter = new ListItemAdapter(getContext(), mMediaItemsProvider);
-        mBrowseList.setAdapter(mMediaAdapter);
-        RecyclerView recyclerView = mBrowseList.findViewById(R.id.recycler_view);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 4);
+        RecyclerView recyclerView = mBrowseList.getRecyclerView();
+        recyclerView.setLayoutManager(gridLayoutManager);
         recyclerView.setVerticalFadingEdgeEnabled(true);
         recyclerView.setFadingEdgeLength(getResources()
-                .getDimensionPixelSize(R.dimen.car_padding_3));
+                .getDimensionPixelSize(R.dimen.car_padding_4));
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(getResources()
+                .getDimensionPixelSize(R.dimen.car_padding_4)));
         TypedValue outValue = new TypedValue();
         getResources().getValue(R.dimen.playback_background_blur_radius, outValue, true);
         mBackgroundBlurRadius = outValue.getFloat();
@@ -140,14 +95,42 @@ public class PlaybackFragment extends Fragment {
     public void onStart() {
         super.onStart();
         mModel.start();
-        mMediaAdapter.start();
+        if (mMediaSource != null) {
+            mMediaSource.subscribe(this);
+        }
+        if (mBrowseAdapter != null) {
+            mBrowseAdapter.start();
+        }
     }
 
     @Override
     public void onStop() {
         super.onStop();
         mModel.stop();
-        mMediaAdapter.stop();
+        if (mMediaSource != null) {
+            mMediaSource.unsubscribe(this);
+        }
+        if (mBrowseAdapter != null) {
+            mBrowseAdapter.stop();
+        }
+    }
+
+    @Override
+    public void onPlaybackStateChanged() {
+        updateState();
+    }
+
+    @Override
+    public void onSourceChanged() {
+        updateState();
+        updateMetadata();
+        updateAccentColor();
+        updateBrowse();
+    }
+
+    @Override
+    public void onMetadataChanged() {
+        updateMetadata();
     }
 
     private void updateState() {
@@ -159,6 +142,10 @@ public class PlaybackFragment extends Fragment {
             mSeekbar.removeCallbacks(mSeekBarRunnable);
         }
         mBrowseList.setVisibility(mModel.hasQueue() ? View.VISIBLE : View.GONE);
+
+        if (mBrowseAdapter != null) {
+            mBrowseAdapter.setQueue(mModel.getQueue(), mModel.getQueueTitle());
+        }
     }
 
     private void updateMetadata() {
@@ -186,7 +173,8 @@ public class PlaybackFragment extends Fragment {
     }
 
     private void updateAccentColor() {
-        int color = mModel.getAccentColor();
+        int defaultColor = getResources().getColor(android.R.color.background_dark, null);
+        int color = mModel.getMediaSource().getAccentColor(defaultColor);
         mSeekbar.getProgressDrawable().setColorFilter(color, PorterDuff.Mode.SRC_IN);
     }
 
@@ -223,7 +211,75 @@ public class PlaybackFragment extends Fragment {
     }
 
     private void updateBrowse() {
+        MediaSource newSource = mModel.getMediaSource();
+        if (Objects.equals(mMediaSource, newSource)) {
+            return;
+        }
+        if (mMediaSource != null) {
+            mMediaSource.unsubscribe(this);
+        }
+        mMediaSource = newSource;
+        mMediaSource.subscribe(this);
         MediaManager.getInstance(getContext())
-                .setMediaClientComponent(mModel.getMediaBrowseServiceComponent());
+                .setMediaClientComponent(mMediaSource.getBrowseServiceComponentName());
+    }
+
+    @Override
+    public void onBrowseConnected(MediaBrowser mediaBrowser) {
+        if (mBrowseAdapter != null) {
+            mBrowseAdapter.stop();
+            mBrowseAdapter = null;
+        }
+        if (mediaBrowser == null) {
+            mBrowseList.setVisibility(View.GONE);
+            // TODO(b/77647430) implement intermediate states.
+            return;
+        }
+        mBrowseAdapter = new BrowseAdapter(getContext(), mediaBrowser, null,
+                ContentForwardStrategy.DEFAULT_STRATEGY);
+        mBrowseList.setAdapter(mBrowseAdapter);
+        mBrowseAdapter.registerObserver(this);
+        mBrowseAdapter.start();
+    }
+
+    @Override
+    public void onBrowseDisconnected() {
+        mBrowseAdapter.stop();
+    }
+
+    @Override
+    public void onDirty() {
+        mBrowseAdapter.update();
+        if (mBrowseAdapter.getItemCount() > 0) {
+            mBrowseList.setVisibility(View.VISIBLE);
+        } else {
+            mBrowseList.setVisibility(View.GONE);
+            // TODO(b/77647430) implement intermediate states.
+        }
+    }
+
+    @Override
+    public void onPlayableItemClicked(MediaItemMetadata item) {
+        mModel.onPlayItem(item.getId());
+    }
+
+    @Override
+    public void onBrowseableItemClicked(MediaItemMetadata item) {
+        // TODO(b/77527398): Drill down in the navigation.
+    }
+
+    @Override
+    public void onMoreButtonClicked(MediaItemMetadata item) {
+        // TODO(b/77527398): Drill down in the navigation
+    }
+
+    @Override
+    public void onQueueTitleClicked() {
+        // TODO(b/77527398): Show full queue
+    }
+
+    @Override
+    public void onQueueItemClicked(MediaItemMetadata item) {
+        mModel.onSkipToQueueItem(item.getQueueId());
     }
 }
